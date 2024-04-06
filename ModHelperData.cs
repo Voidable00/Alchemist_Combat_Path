2e533e94c4dd6db942822ac58991c7fe0df51d80
Voidable00 <166196924+Voidable00@users.noreply.github.com>
2024-04-06 00:31:17 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,12 +4,12 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "37.0";
     public const string Version = "0.0.0";
-    public const string Name = "FourthPathTemplate";
+    public const string Name = "AlchemistCombatPath";
 
-    public const string Description = "Adds a fourth path";
+    public const string Description = "Adds a fourth path to the Alchemist";
 
-    public const string RepoOwner = "Derpy-Jacob-903"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "FourthPathTemplate"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Voidable00"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Alchemist_Combat_Path"; // TODO add your repo name here, also in the download url in README.md
 
     public const string Dependencies = "doombubbles/paths-plus-plus";
 }
